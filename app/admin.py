b15ea7e59ec308ebfb24b7ed96ad2f3b12b74cbd
chucklapress <charleslapress@gmail.com>
2017-08-09 17:56:53 -0400

    added db models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from app.models import HeadlineArticle
+from app.models import LocalArticle
+from app.models import SportsArticle
+from app.models import BusinessArticle
 
 # Register your models here.
+admin.site.register(HeadlineArticle)
+admin.site.register(LocalArticle)
+admin.site.register(SportsArticle)
+admin.site.register(BusinessArticle)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class HeadlineArticle(models.Model):
+    top_stories_today = models.TextField(max_length=None)
+    second_lede_today = models.TextField(max_length=None)
+    third_lede_today = models.TextField(max_length=None)
+    def __str__(self):
+        return self.top_stories_today
+
+class LocalArticle(models.Model):
+    top_local_today = models.TextField(max_length=None)
+    other_local_today = models.TextField(max_length=None)
+    def __str__(self):
+        return self.top_local_today
+
+class SportsArticle(models.Model):
+    top_sports_today = models.TextField(max_length=None)
+    other_sports_today = models.TextField(max_length=None)
+    def __str__(self):
+        return self.top_sports_today
+
+class BusinessArticle(models.Model):
+    top_business_today = models.TextField(max_length=None)
+    other_business_today = models.TextField(max_length=None)
+    def __str__(self):
+        return self.top_business_today

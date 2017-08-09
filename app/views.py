c34d45a7134c3a42501e0bd54551511bc1acf6e9
chucklapress <charleslapress@gmail.com>
2017-08-09 13:12:03 -0400

    changes

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView,View,CreateView,ListView
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django import forms
+
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
